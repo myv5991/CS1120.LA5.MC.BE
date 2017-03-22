ab0f43027e4a33ba4d2d4d1d2d00e3041238f263
Mogler <mikemanx2@gmail.com>
2017-03-22 11:46:34 -0400

    Updated iArea, Area

--- CS1120MCBELA5/src/edu/wmich/cs1120/la5/Area.java
+++ CS1120MCBELA5/src/edu/wmich/cs1120/la5/Area.java
@@ -1,5 +1,65 @@
 package edu.wmich.cs1120.la5;
 
+import edu.wmich.cs1120.la5.gui.MainWindow;
+import edu.wmich.cs1120.la5.TerrainScanner;
+
+
 public class Area implements IArea{
+	private double basicEnergyCost;
+	private double elevation;
+	private double radiation;
+	
+	/**
+	 * gets basic energy cost
+	 */
+	public double getBasicEnergyCost() {
+		// TODO Auto-generated method stub
+		return basicEnergyCost;
+	}
+
+	/**
+	 * sets basic energy cost for class
+	 */
+	public void setBasicEnergyCost(double basicEnergyCost) {
+		// TODO Auto-generated method stub
+		this.basicEnergyCost = basicEnergyCost;
+	}
+	/**
+	 * gets elevation
+	 */
+	public double getElevation() {
+		// TODO Auto-generated method stub
+		return elevation;
+	}
+	/**
+	 * sets elevation for class
+	 */
+	public void setElevation(double elevation) {
+		// TODO Auto-generated method stub
+		this.elevation = elevation;
+	}
+	/**
+	 * gets radiation
+	 */
+	public double getRadiation() {
+		// TODO Auto-generated method stub
+		return radiation;
+	}
+	/**
+	 * sets radiation for class
+	 */
+	public void setRadiation(double radiation) {
+		// TODO Auto-generated method stub
+		this.radiation = radiation;
+	}
+
+	/**
+	 * extra for calc energy cost
+	 */
+	public double calcConsumedEnergy() {
+		double energy_cost =0; // temp for no errors
+		
+		return energy_cost;
+	}
 
 }
--- CS1120MCBELA5/src/edu/wmich/cs1120/la5/IArea.java
+++ CS1120MCBELA5/src/edu/wmich/cs1120/la5/IArea.java
@@ -1,5 +1,15 @@
 package edu.wmich.cs1120.la5;
 
-public interface IArea {
+import edu.wmich.cs1120.la5.gui.MainWindow;
+import edu.wmich.cs1120.la5.TerrainScanner;
 
+public interface IArea {
+	double getBasicEnergyCost();
+	void setBasicEnergyCost(double basicEnergyCost);
+	double getElevation();
+	void setElevation(double elevation);
+	double getRadiation();
+	void setRadiation(double radiation);
+	double calcConsumedEnergy();
 }
+
