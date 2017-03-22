78b8cc2467b7d167ef93c3d6e11cb66192ba39b3
benec <benec@10.80.141.235>
2017-03-22 12:43:22 -0400

    provide implementation for class ExpressionFactory

--- CS1120MCBELA5/src/edu/wmich/cs1120/la5/ExpressionFactory.java
+++ CS1120MCBELA5/src/edu/wmich/cs1120/la5/ExpressionFactory.java
@@ -1,5 +1,8 @@
 package edu.wmich.cs1120.la5;
 
 public class ExpressionFactory {
-
+	public IExpression getExpression(char op, int val1, int val2){
+		IExpression expression = new BinaryExpression(op, val1, val2);
+		return expression;
+	}
 }
