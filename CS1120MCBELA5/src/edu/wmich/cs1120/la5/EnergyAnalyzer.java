43d978ed199e58e893bd59a7f235d9e352cdfa3f
Mogler <mikemanx2@gmail.com>
2017-03-22 13:06:47 -0400

    Setup Energy Analyzer

--- CS1120MCBELA5/src/edu/wmich/cs1120/la5/EnergyAnalyzer.java
+++ CS1120MCBELA5/src/edu/wmich/cs1120/la5/EnergyAnalyzer.java
@@ -8,6 +8,7 @@ import edu.wmich.cs1120.la5.TerrainScanner;
 public class EnergyAnalyzer implements IRover{
 
 	ArrayList<IArea> list;
+	 String analysisLocal;
 	
 	@Override
 	public ArrayList<IArea> getPath() {
@@ -19,28 +20,36 @@ public class EnergyAnalyzer implements IRover{
 	public void setPath(ArrayList<IArea> path) {
 
  list = path;
+
 		
 	}
 
 	@Override
 	public void analyzePath() {
-		// TODO Auto-generated method stub
+		double totalenergy = 0;
 		
+		for(int i = 0; i < list.size(); i++){
+			IArea temp = new Area();
+			temp = list.get(i);
+		totalenergy = totalenergy + temp.calcConsumedEnergy();	
+		}
+		
+		analysisLocal = Double.toString(totalenergy);
 	}
 
 	@Override
 	public String getAnalysis() {
 		// TODO Auto-generated method stub
-		return null;
+		return analysisLocal;
 	}
 
 	@Override
 	public void setAnalysis(String analysis) {
-		// TODO Auto-generated method stub
+		analysisLocal = analysis;
 		
 	}
 	public String toString(){
-		return "Energy";
+		return "EnergyAnalyzer";
 	}
 
 }
