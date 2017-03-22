46ef4df55f0e1effd8670e783383c05f226e0ce2
Mogler <mikemanx2@gmail.com>
2017-03-22 11:59:17 -0400

    Updated IMapCreator With Methods and implemented them in maptxt and mapdata

--- CS1120MCBELA5/src/edu/wmich/cs1120/la5/IMapCreator.java
+++ CS1120MCBELA5/src/edu/wmich/cs1120/la5/IMapCreator.java
@@ -1,5 +1,16 @@
 package edu.wmich.cs1120.la5;
 
-public interface IMapCreator {
+import edu.wmich.cs1120.la5.gui.MainWindow;
+import edu.wmich.cs1120.la5.TerrainScanner;
 
+public interface IMapCreator {
+void scanTerrain(String fileName, int threshold) throws IOException;
+
+// Read from the file, set up the IArea[10][10] and set the
+//terrain for the object of TerrainScanner.
+
+TerrainScanner getScanner();
+		
+void setScanner(TerrainScanner scanner);
+		
 }
