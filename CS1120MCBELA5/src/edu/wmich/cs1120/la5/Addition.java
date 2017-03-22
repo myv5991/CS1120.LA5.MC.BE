ceef220045b5c81c8c6e2e530eff6b937c8b70bb
benec <benec@10.80.141.235>
2017-03-22 12:12:44 -0400

    provide implementation for classes Addition and Subtraction

--- CS1120MCBELA5/src/edu/wmich/cs1120/la5/Addition.java
+++ CS1120MCBELA5/src/edu/wmich/cs1120/la5/Addition.java
@@ -1,5 +1,7 @@
 package edu.wmich.cs1120.la5;
 
-public class Addition implements IExpression{
-
+public class Addition implements IOperation{
+	public Integer add(int val1, int val2){
+		return val1 + val2;
+	}
 }
--- CS1120MCBELA5/src/edu/wmich/cs1120/la5/Subtraction.java
+++ CS1120MCBELA5/src/edu/wmich/cs1120/la5/Subtraction.java
@@ -1,5 +1,7 @@
 package edu.wmich.cs1120.la5;
 
 public class Subtraction {
-
+	public Integer subtract(int val1, int val2){
+		return val1 + val2;
+	}
 }
