0bb5c019f05028a60d9f131a717902903fee39c2
benec <benec@10.80.141.235>
2017-03-22 11:45:10 -0400

    provide implementation for class Literal

--- CS1120MCBELA5/src/edu/wmich/cs1120/la5/Literal.java
+++ CS1120MCBELA5/src/edu/wmich/cs1120/la5/Literal.java
@@ -1,8 +1,12 @@
 package edu.wmich.cs1120.la5;
 
 public class Literal implements IExpression{
-	
+	int val;
 	public Integer getValue(){
-		return 1;
+		return this.val;
+	}
+	
+	public Literal(int val){
+		this.val = val;
 	}
 }
