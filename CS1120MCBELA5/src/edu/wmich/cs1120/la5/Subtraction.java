82ceb9e0152bb30157e64a3efb7115d23ab2ce1c
benec <benec@10.80.141.235>
2017-03-22 12:32:00 -0400

    corrected an error in class Subtraction and integrated Addition and Subtraction classes into class BinaryExpression

--- CS1120MCBELA5/src/edu/wmich/cs1120/la5/Subtraction.java
+++ CS1120MCBELA5/src/edu/wmich/cs1120/la5/Subtraction.java
@@ -1,6 +1,6 @@
 package edu.wmich.cs1120.la5;
 
-public class Subtraction {
+public class Subtraction implements IOperation{
 	public Integer subtract(int val1, int val2){
 		return val1 + val2;
 	}
